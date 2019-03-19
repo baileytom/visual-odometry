f9729b9703547bb0dc81d6a8f6e5b458bd032703
Thomas Bailey <baileytb7@gmail.com>
2019-03-18 20:19:50 -0400

    Validation, testing data segmentation. Trained model on small dataset to ~65% on testing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import scipy
+import keras
 import tensorflow as tf
 import pathlib
 import random
@@ -10,6 +11,7 @@ import bisect
 import imageio
 import numpy as np
 from tensorflow.python.keras import utils
+from sklearn.model_selection import train_test_split
 import cv2
 
 # Preprocessing, the best part of any project
@@ -59,12 +61,58 @@ def image_to_np(filename):
     image = cv2.resize(image, dsize=(32, 32), interpolation=cv2.INTER_CUBIC)
     return image
     
-images = [image_to_np(f) for f in image_paths]
+images = [image_to_np(f)/255 for f in image_paths]
 
+# Convert images & positions to their delta values (differences between adjacent frames & positions)
+d_images = []
+d_positions = []
+for i in range(len(images)-1):    
+    d_image = images[i+1] - images[i]
+    #scipy.misc.imsave('image0.png', images[i])
+    #scipy.misc.imsave('image1.png', images[i+1])
+    #scipy.misc.imsave('dimage.png', d_image)
+    d_images.append(d_image)
+    d_position = positions[i+1] - positions[i]
+    d_positions.append(d_position)
 
 img_x, img_y = 32, 32
 input_shape = (img_x, img_y, 3)
 
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(
+        32,
+        (3, 3),
+        padding='same',
+        activation='relu',
+        input_shape=(32,32,3)
+    ),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Conv2D(
+        32,
+        (3, 3),
+        padding='same',
+        activation='relu'
+    ),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+    tf.keras.layers.Conv2D(
+        64,
+        (3, 3),
+        padding='same',
+        activation='relu'
+    ),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0,2),
+    tf.keras.layers.Dense(
+        512,
+        activation='relu',
+        kernel_constraint=keras.constraints.maxnorm(3)
+    ),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(7)
+])
+
+"""
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(
         32,
@@ -82,21 +130,26 @@ model = tf.keras.models.Sequential([
     ),
     tf.keras.layers.Dense(7)
 ])
+"""
 
 model.compile(optimizer='adam',
               loss='mean_squared_error',
               metrics=['accuracy'])
 
 
-images = np.array(images)
-positions = np.array(positions)
+d_images = np.array(d_images)
+d_positions = np.array(d_positions)
 
-model.fit(images, positions, epochs=100)
+# Split into training, testing, & validation sets
 
-img = np.expand_dims(images[0], axis=0)
+x_train, x_test, y_train, y_test = train_test_split(d_images, d_positions, test_size=0.2)
+x_test, x_valid, y_test, y_valid = train_test_split(x_test, y_test, test_size=0.2)
 
-print(model.predict(img))
-print(positions[0])
+for item in d_positions:
+    print(item)
 
+model.fit(x_train, y_train, validation_data=(x_valid, y_valid), epochs=100)
 
-#loss, acc = model.evaluate(x_test, y_test)
+loss, acc = model.evaluate(x_test, y_test)
+
+print(loss, acc)
